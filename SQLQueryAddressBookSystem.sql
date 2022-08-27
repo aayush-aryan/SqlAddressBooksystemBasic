4f0f0827b37203b292f412f244b4477a2a959f9e
aayush-aryan <log2aayusharyan@gmail.com>
2022-08-27 06:29:49 +0530

    iserting records in table

--- SQLQueryAddressBookSystem.sql
+++ SQLQueryAddressBookSystem.sql
@@ -14,3 +14,9 @@ PhoneNumber varchar(15) not null,
 Email varchar(50) not null
 )
 select* from AddressBook
+------UC-3------
+insert into AddressBook values
+('Aayush','Kumar','Sector75','Noida','UttarPardesh','800020','123456789','aayush@gmail.com'),
+('Aryan','Raj','Sector14','Grugram','Haryana','800020','1234509876','aryan@yahoo.com')
+
+select* from AddressBook
