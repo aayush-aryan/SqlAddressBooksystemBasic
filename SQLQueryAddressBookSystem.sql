2ea1889494806ca2baae98be5bc0b1eea126c44e
aayush-aryan <log2aayusharyan@gmail.com>
2022-08-27 06:17:39 +0530

    creating table in DB

--- SQLQueryAddressBookSystem.sql
+++ SQLQueryAddressBookSystem.sql
@@ -1,3 +1,16 @@
 create database AddressBookService
 select name from sys.databases
 use AddressBookService
+----Uc2-----
+create table AddressBook
+(
+FirstName varchar(100) not null,
+LastName varchar(100) not null,
+Address varchar(200) not null,
+City varchar(50) not null,
+State varchar(50) not null,
+Zipcode varchar(20) not null,
+PhoneNumber varchar(15) not null,
+Email varchar(50) not null
+)
+select* from AddressBook
